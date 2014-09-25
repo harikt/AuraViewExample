af7ac57690c838bebc9a62ff6df5a8c55c913a83
Hari KT <kthari85@gmail.com>
2014-09-25 10:58:55 +0530

    Add directory iterator to iterate the files and load automatically

--- view.php
+++ view.php
@@ -8,11 +8,26 @@ $view_factory = new \Aura\View\ViewFactory;
 $view = $view_factory->newInstance($helpers);
 
 $view_registry = $view->getViewRegistry();
-$view_registry->set('browse', __DIR__ . '/templates/views/browse.php');
+$directory = __DIR__ . '/templates/views/';
+$iterator = new DirectoryIterator($directory);
+foreach ($iterator as $fileinfo) {
+    if ($fileinfo->isFile()) {
+        $view_registry->set($fileinfo->getBasename('.php'), $fileinfo->getPathname());
+    }
+}
 
 $layout_registry = $view->getLayoutRegistry();
 $layout_registry->set('default', __DIR__ . '/templates/layouts/default.php');
-
+// for those who are lazy ;-)
+/*
+$directory = __DIR__ . '/templates/layouts/';
+$iterator = new DirectoryIterator($directory);
+foreach ($iterator as $fileinfo) {
+    if ($fileinfo->isFile()) {
+        $layout_registry->set($fileinfo->getBasename('.php'), $fileinfo->getPathname());
+    }
+}
+*/
 $view->setData(array('name' => 'Aura'));
 
 $view->setView('browse');
